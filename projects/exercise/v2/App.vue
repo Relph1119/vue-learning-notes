bcc9aa77186101582cc27b73e9bb69f57a8bed98
Relph1119 <huruifeng1202@163.com>
2023-06-14 23:19:21 +0800

    完成练习第2版：组件拆分，并使用v-for进行展示

---
+++ projects/exercise/v2/App.vue
@@ -0,0 +1,111 @@
+<script setup>
+import {reactive, ref} from "vue";
+import TabItem from "./components/TabItem.vue";
+
+const current = ref(0) //0表示球员,1表示球队
+
+const players = reactive([
+  {
+    name: "梅西",
+    img: "/images/messi.png",
+    rate: 1,
+    hot: 433760
+  },
+  {
+    name: "C罗",
+    img: "/images/ronaldo.png",
+    rate: 2,
+    hot: 333760
+  },
+  {
+    name: "内马尔",
+    img: "/images/neymar.png",
+    rate: 3,
+    hot: 333760
+  }
+])
+
+const teams = reactive([
+   {
+    name: "法国",
+    img: "/images/法国.jpg",
+    rate: 1,
+    hot: 333760
+  },
+  {
+    name: "巴西",
+    img: "/images/巴西.jpg",
+    rate: 2,
+    hot: 233760
+  },
+  {
+    name: "荷兰",
+    img: "/images/荷兰.jpg",
+    rate: 3,
+    hot: 133760
+  }
+])
+
+const playerMaxHot = players[0].hot
+const teamMaxHot = teams[0].hot
+</script>
+
+<template>
+  <div class="tab-wrapper">
+    <header class="tab-head">
+      <div @click="current = 0" class="tab-button" :class="{active:current === 0}">热门球员</div>
+      <div @click="current = 1" class="tab-button" :class="{active:current === 1}">热门球队</div>
+    </header>
+    <div class="main">
+      <div v-show="current === 0">
+        <!-- 球员-->
+        <div class="tab-list">
+          <TabItem v-for="player in players" :item="player" :maxHot="playerMaxHot"></TabItem>
+        </div>
+      </div>
+      <div v-show="current === 1">
+        <!-- 球队 -->
+        <div class="tab-list">
+          <TabItem v-for="team in teams" :item="team" :maxHot="teamMaxHot"></TabItem>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tab-wrapper {
+  width: 800px;
+  padding: 20px;
+  background-color: rgb(45, 83, 211);
+}
+
+.tab-head {
+  display: flex;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.tab-button {
+  background-color: #fff;
+  font-size: 30px;
+  padding: 10px 0;
+  text-align: center;
+  flex: auto;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+.tab-button:not(.active):hover {
+  color: rgb(187, 3, 52);
+}
+
+.active {
+  background-color: rgb(187, 3, 52);
+  color: #fff;
+}
+
+.tab-list {
+  margin: 20px;
+}
+</style>
---
+++ projects/exercise/v2/components/TabItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {reactive} from "vue"
+import Photo from "./Photo.vue";
+import HotBar from "./HotBar.vue";
+/*
+子组件中的数据通常不会在子组件中直接定义，通常在创建组件实例时确定。
+父组件可以通过props将数据传递给子组件
+ */
+const props = defineProps(["item", "maxHot"]);
+const item = props.item;
+</script>
+
+<template>
+  <div class="tab-item">
+    <!-- 图片-->
+    <Photo :src="item.img" :alt="item.name" :rate="item.rate"></Photo>
+    <!-- 描述  -->
+    <div class="desc">
+      <span class="name">{{item.name}}</span>
+      <HotBar :hot="item.hot" :maxHot="props.maxHot"></HotBar>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tab-item {
+  display: flex;
+  margin-bottom: 40px;
+}
+
+.desc {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-evenly;
+  font-size: 30px;
+  color: #fff;
+  margin-left: 30px;
+  flex: auto;
+}
+
+
+</style>
