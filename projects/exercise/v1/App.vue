d21c34f1eddabda603ffb425ea200edda56ae8ee
Relph1119 <huruifeng1202@163.com>
2023-06-14 23:15:06 +0800

    完成第1版练习-热门球员和热门球队的展示

---
+++ projects/exercise/v1/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import {reactive, ref} from "vue";
+import TabItem from "./components/TabItem.vue";
+const current = ref(0) //0表示球员,1表示球队
+
+const player = reactive({
+  name: "梅西",
+  img: "/images/messi.png",
+  rate: 1,
+  hot: 433760
+})
+
+const team = reactive({
+  name: "法国",
+  img: "/images/法国.jpg",
+  rate: 1,
+  hot: 333760
+})
+</script>
+
+<template>
+  <div class="tab-wrapper">
+    <header class="tab-head">
+      <div @click="current = 0" class="tab-button" :class="{active:current === 0}">热门球员</div>
+      <div @click="current = 1" class="tab-button" :class="{active:current === 1}">热门球队</div>
+    </header>
+    <div class="main">
+      <div v-show="current === 0">
+        <!-- 球员-->
+        <div class="tab-list">
+          <TabItem :item="player"></TabItem>
+        </div>
+      </div>
+      <div v-show="current === 1">
+      <!-- 球队 -->
+        <div class="tab-list">
+          <TabItem :item="team"></TabItem>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tab-wrapper {
+  width: 800px;
+  padding: 20px;
+  background-color: rgb(45,83,211);
+}
+
+.tab-head {
+  display: flex;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.tab-button {
+  background-color: #fff;
+  font-size: 30px;
+  padding: 10px 0;
+  text-align: center;
+  flex: auto;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+.tab-button:not(.active):hover {
+  color: rgb(187, 3, 52);
+}
+
+.active {
+  background-color: rgb(187, 3, 52);
+  color: #fff;
+}
+
+.tab-list {
+  margin: 20px;
+}
+</style>
---
+++ projects/exercise/v1/components/TabItem.vue
@@ -0,0 +1,97 @@
+<script setup>
+import {reactive} from "vue"
+ /*
+ 子组件中的数据通常不会在子组件中直接定义，通常在创建组件实例时确定。
+ 父组件可以通过props将数据传递给子组件
+
+  */
+const props = defineProps(["item"]);
+const item = props.item;
+</script>
+
+<template>
+  <div class="tab-item">
+    <!-- 图片-->
+    <div class="photo">
+      <img :src="item.img" :alt="item.name">
+      <span>{{item.rate}}</span>
+    </div>
+    <!-- 描述  -->
+    <div class="desc">
+      <span class="name">{{item.name}}</span>
+      <div class="hot-bar">
+        <div class="inner">{{item.hot}}热度</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped>
+.tab-item {
+  display: flex;
+  margin-bottom: 40px;
+}
+
+/* 容器 */
+.photo {
+  width: 150px;
+  background-color: #fff;
+  border-radius: 20px;
+  overflow: hidden;
+  position: relative;
+
+}
+
+/* 图片 */
+.photo img {
+  width: 100%;
+  vertical-align: bottom;
+}
+
+/* 排名 */
+.photo span {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background-color: rgb(245, 102, 1);
+  top: 0;
+  left: 0;
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom-right-radius: 20px;
+  border-top-left-radius: 20px;
+}
+
+.desc {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-evenly;
+  font-size: 30px;
+  color: #fff;
+  margin-left: 30px;
+  flex: auto;
+}
+
+.hot-bar {
+  background-color: rgb(3, 37, 103);
+  border-radius: 20px;
+  text-indent: 0.5em;
+  overflow: hidden;
+}
+
+.inner {
+  border-radius: 20px;
+  background-image: linear-gradient(
+      90deg,
+      rgb(187, 3, 52) 50%,
+      rgb(66, 2, 12));
+  width: 10%;
+  white-space: nowrap;
+}
+</style>
